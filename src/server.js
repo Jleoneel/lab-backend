ef9185ff9269ae60b41559a38dd112636526c9fa
Jleoneel <jarrunategui3139@utm.edu.ec>
2026-05-09 16:08:25 -0500

    fix deployv2

--- src/server.js
+++ src/server.js
@@ -2,5 +2,4 @@ require("dotenv").config();
 const { app } = require("./app");
 
 const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {});
+app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
